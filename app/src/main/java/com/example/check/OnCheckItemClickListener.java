0a04e7797feabed38f77d4323e5069e515b7483c
gyuri127 <gyurih127@gmail.com>
2022-11-29 19:45:45 +0900

    1

---
+++ app/src/main/java/com/example/check/OnCheckItemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.check;
+
+import android.view.View;
+
+public interface OnCheckItemClickListener {
+    public void onItemClick(CheckAdapter.CheckViewHolder holder, View view, int position);
+}
